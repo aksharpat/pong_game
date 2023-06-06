7b4fa589733f5aa063ecd3f356aefee310bf5c88
aksharpat <akshar0308@gmail.com>
2023-06-05 22:41:57 -0500

    Code for the paddles

---
+++ Paddle.js
@@ -0,0 +1,29 @@
+const SPEED = .02
+
+export default class Paddle {
+    constructor(paddleElm) {
+        this.paddleElm = paddleElm
+        this.reset()
+    }
+
+    get position() {
+        return parseFloat(getComputedStyle(this.paddleElm).getPropertyValue("--position"))
+
+    }
+    set position(value) {
+        this.paddleElm.style.setProperty("--position", value)
+
+    }
+
+    rect() {
+        return this.paddleElm.getBoundingClientRect()
+    }
+
+    reset() {
+        this.position = 50
+    }
+
+    update(delta, ballHeight) {
+        this.position += SPEED * delta * (ballHeight - this.position)
+    }
+}
